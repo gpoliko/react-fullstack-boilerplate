31b9ded5076aa812722dc1260b85b18e1c3e8197
gpoliko <60165179+gpoliko@users.noreply.github.com>
2020-11-14 13:59:03 +1300

    Update api.js - currently local api

---
+++ client/api.js
@@ -0,0 +1,9 @@
+import request from 'superagent'
+
+const widgetUrl = 'http://localhost:3000/api/v1/widgets'
+
+export function getWidgets () {
+  return request
+    .get(widgetUrl)
+    .then(response => response.body)
+}
