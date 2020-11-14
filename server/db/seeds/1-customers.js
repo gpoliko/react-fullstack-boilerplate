d9aabcf8f3555d8ec5e85c7fc3d13c7533295afb
gpoliko <60165179+gpoliko@users.noreply.github.com>
2020-11-14 15:09:55 +1300

    add order data to orders migration file

--- server/db/seeds/1-customers.js
+++ server/db/seeds/1-customers.js
@@ -1,15 +1,14 @@
-
-exports.seed = function(knex) {
+exports.seed = knex => {
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
+  return knex('Customers').del()
+    .then(() => {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, full_name: 'Alfreds Futterkiste', phone: 0211234567, country: 'Germany'},
-        {id: 2, full_name: 'Madelaine Maynard', phone: 0211234567, country: 'Malta'},
-        {id: 3, full_name: 'Yash Hopper', phone: 0211234567, country: 'Canada'},
-        {id: 4, full_name: 'Lena Carpenter', phone: 0211234567, country: 'Ukraine'},
-        {id: 5, full_name: 'Jaeden Bush', phone: 0211234567, country: 'United States of America'},
-      ]);
-    });
-};
+      return knex('Customers').insert([
+        {id: 1, full_name: 'Alfreds Futterkiste', phone: 9954122259, country: 'Germany'},
+        {id: 2, full_name: 'Madelaine Maynard', phone: 8995920575, country: 'Malta'},
+        {id: 3, full_name: 'Yash Hopper', phone: 2882911894, country: 'Canada'},
+        {id: 4, full_name: 'Lena Carpenter', phone: 2508376713, country: 'Ukraine'},
+        {id: 5, full_name: 'Jaeden Bush', phone: 4438597747, country: 'United States of America'},
+      ])
+    })
+}
